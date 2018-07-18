899c102f546d34e3f254190a592a6971f916a5cb
zhujunjian <zhujunjian@shuidihuzhu.com>
2018-07-18 14:50:48 +0800

    readfile

--- src/main/java/test1/Test1.java
+++ src/main/java/test1/Test1.java
@@ -1,5 +1,42 @@
 package test1;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 
 public class Test1 {
-    olalal
+    public static String txt2string(File file) {
+        StringBuilder result = new StringBuilder();
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String s = null;
+            while ((s = br.readLine()) != null)
+            {
+                result.append(System.lineSeparator() + s);
+            }
+            br.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return result.toString();
+
+        }
+
+
+
+
+    public static void main(String[] args) {
+        File file = new File("E:/yo.txt");
+        System.out.println(txt2string(file));
+
+
+
+
+
+    }
+
 }
